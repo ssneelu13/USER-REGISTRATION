1be3df7aa810d8c890d7b2059e7e491dda1d2461
Neelufer <ssneelu13@gmail.com>
2023-11-24 10:57:26 +0530

    WELCOME TO USER REGISTRATION PROGRAM

---
+++ src/userregistration/UserRegistration.java
@@ -0,0 +1,11 @@
+package userregistration;
+public class UserRegistration {
+    public static void main(String[]args){
+
+        System.out.println("WELCOME TO USER REGISTRATION PROGRAM");
+       
+
+        
+
+    }
+}
